e88658a257c71d67e7a62d38ca91c32cc26fad27
Dawelo1 <gieromaniak.gry@wp.pl>
2025-11-23 05:47:55 +0100

    poprawiona widocznosc w panelu admin

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,36 @@
 from django.contrib import admin
-from .models import Group, Child, Payment, Post
+from .models import Group, Child, Payment, Post, Attendance
 
 # Prosta rejestracja - pozwoli dodawać/edytować elementy
 admin.site.register(Group)
-admin.site.register(Child)
 admin.site.register(Payment)
 admin.site.register(Post)
+
+# Konfiguracja wyświetlania Dzieci
+class ChildAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'group') # Co widać w kolumnach
+    list_filter = ('group',) # Filtr grup po prawej stronie
+    search_fields = ('last_name', 'first_name') # Pasek wyszukiwania
+
+admin.site.register(Child, ChildAdmin)
+
+# --- TU JEST KONFIGURACJA ATTENDANCE (OBECNOŚCI) ---
+class AttendanceAdmin(admin.ModelAdmin):
+    # Kolumny widoczne na liście
+    list_display = ('date', 'child', 'group_name', 'status')
+    
+    # Filtry po prawej stronie (po dacie, statusie i grupie dziecka)
+    list_filter = ('date', 'status', 'child__group')
+    
+    # Pozwala zmieniać status bezpośrednio na liście (szybkie odklikiwanie)
+    list_editable = ('status',)
+    
+    # Domyślne sortowanie (najnowsze daty na górze)
+    ordering = ('-date', 'child__last_name')
+
+    # Pomocnicza metoda, żeby wyświetlić nazwę grupy (bo grupa jest w modelu Child, a nie Attendance)
+    def group_name(self, obj):
+        return obj.child.group.name
+    group_name.short_description = 'Grupa'
+
+admin.site.register(Attendance, AttendanceAdmin)
