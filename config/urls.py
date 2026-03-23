4ed9a9621ead0bcaf2d5b7289c12e827d1cf9d63
Dawelo1 <gieromaniak.gry@wp.pl>
2026-03-23 01:40:41 +0100

    serve Vite assets directly in production

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
+from django.views.static import serve
 from django.urls import re_path
 from django.conf import settings
 from django.conf.urls.static import static
@@ -13,6 +14,8 @@ admin.site.index_title = "Panel Zarządzania"
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html'), name='spa-root'),
+    re_path(r'^assets/(?P<path>.*)$', serve, {'document_root': settings.FRONTEND_DIST_DIR / 'assets'}),
+    path('favicon.png', serve, {'path': 'favicon.png', 'document_root': settings.FRONTEND_DIST_DIR}),
     path('admin/', admin.site.urls),
     path('api/', include('core.urls')),
     path('api/communication/', include('communication.urls')),
